247277598746df04d38866e7771772cef62a2d33
Polaris <imll1@icloud.com>
2024-03-24 00:59:57 -0400

    added dark mode option (so far just to 3d) and another IC

--- generalNbod3d.m
+++ generalNbod3d.m
@@ -3,14 +3,14 @@ clc; clear; close all;
 
 %---ial Conditions-----------------------------------------------------
 mE = 5.972e24; % Earth mass
-m = [mE 0.7*mE 0.9*mE 0.8*mE, 0.75*mE, 3*mE];
-r1 = [0;0;0];           v1 = [0;0;0];
-r2 = [0;-9e8;9e8];      v2 = [100;-300;-900];
-r3 = [13e8;-4e8;1e8];   v3 = [-300;-200;300];
-r4 = [-12e8;5e7;-9e8];  v4 = [-800;200;400];
-r5 = [1e9;1.2e9;-6e8];  v5 = [80;-900;50];
-r6 = [-1e8;7e8;1.2e9];    v6 = [400;400;-500];
-sv = [r1;v1;r2;v2;r3;v3;r4;v4;r5;v5;r6;v6];
+m = [mE 0.7*mE 0.9*mE 0.8*mE, 0.75*mE, 0.85*mE];
+r1init = [0;0;0];           v1init = [0;0;0];
+r2init = [0;-9e8;9e8];      v2init = [100;-300;-900];
+r3init = [13e8;-4e8;1e8];   v3init = [-300;-200;300];
+r4init = [-12e8;5e7;-9e8];  v4init = [-800;200;400];
+r5init = [1e9;1.2e9;-6e8];  v5init = [80;-900;50];
+r6init = [-1e8;7e8;1.2e9];    v6init = [400;400;-500];
+svinit = [r1init;v1init;r2init;v2init;r3init;v3init;r4init;v4init;r5init;v5init;r6init;v6init];
 % you can hardcode this. SI base units
 
 %---Sim Times--------------------------------------------------------------
@@ -22,6 +22,7 @@ rotrate = 15; % deg/sec rotate
 
 useCoM = true;  % whether to use CoM as origin
 useTex = true;  % whether to use tex labels (otherwise MATLAB labels)
+useDarkMode = true; % whether to use dark mode
 % Note: search for ANIMATIONCOMMENT for stuff to toggle based on whether
 % animating or not
 
@@ -69,11 +70,11 @@ else
     interp = "tex";
 end
 H = linspace(0,1-1/numbods,numbods)';   % hue
-colors = hsv2rgb([H, ones(size(H)), .75*ones(size(H))]); %color order
+colors = hsv2rgb([H, ones(size(H)), (.75+0.25*useDarkMode)*ones(size(H))]); %color order
 %% Paths
-figure;
+figure('color','w');
 colororder(colors);
-plot3(x, y, z, '-'); set(gcf, "color", 'w');
+plot3(x, y, z, '-'); ax=gca;
 lgtxt = {};
 for idx=1:numbods
     mExp = floor(log10(m(idx))); mCoef = round(m(idx)/10^mExp, 3);
@@ -82,7 +83,14 @@ end
 lg=legend(lgtxt,Interpreter=interp,Location="eastoutside", box="off");
 lg.ItemTokenSize=[10 5]; lg.Title.String="Masses [kg]"; 
 set(gca,'TickLabelInterpreter',interp)
-title(numbods+"-Body Sim", Interpreter=interp); axis equal;grid on;
+ttle=title(numbods+"-Body Sim", Interpreter=interp); axis equal;grid on;
+
+if(useDarkMode)
+    set(gcf, "Color", 'k'); set(gca,'Color','k');
+    set(gca,'GridColor','w');set(gca,'XColor','w');set(gca,'YColor','w');set(gca,'ZColor','w');
+    lg.Title.Color='w'; ax.Title.Color='w'; set(lg,'textcolor','w')
+end
+
 [az, el] = view; xl = xlim; yl=ylim; zl=zlim;
 %% Animation
 figure; set(gcf, "color", 'w');
@@ -96,6 +104,13 @@ for idx=1:numbods
 end
 lg=legend(["", lgtxt],Interpreter=interp,Location="eastoutside", box="off");
 lg.ItemTokenSize=[10 5]; lg.Title.String="Masses [kg]";
+
+if(useDarkMode)
+    set(gcf, "Color", 'k'); set(gca,'Color','k');
+    set(gca,'GridColor','w');set(gca,'XColor','w');set(gca,'YColor','w');set(gca,'ZColor','w');
+    lg.Title.Color='w'; animAx.Title.Color='w'; set(lg,'textcolor','w')
+end
+
 pause; tic; xlim(xl);ylim(yl);zlim(zl);
 animation = VideoWriter("animation3d_"+string(datetime, "yyyy-MM-dd-hhmmss")+".mp4",'MPEG-4');      % ANIMATIONCOMMENT
 animation.FrameRate = fps; animation.Quality=100; open(animation);      % ANIMATIONCOMMENT
