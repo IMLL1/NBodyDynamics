dc6e5796d009d805ae68ae707f547eae7438cc47
Polaris <imll1@icloud.com>
2024-03-23 23:52:38 -0400

    made 2d actually 2d instead of the same as the 3d

--- generalNbod2d.m
+++ generalNbod2d.m
@@ -1,31 +1,25 @@
 %% user input
 clc; clear; close all;
 
-%---Initial Conditions-----------------------------------------------------
-mE = 5.972e24; % Earth mass
-m = [mE 0.7*mE 0.9*mE 0.8*mE, 0.75*mE];
-r1init = [0;0;0];           v1init = [0;0;0];
-r2init = [0;-9e8;9e8];      v2init = [100;-300;-900];
-r3init = [13e8;-4e8;1e8];   v3init = [-300;-200;300];
-r4init = [-12e8;5e7;-9e8];  v4init = [-800;200;400];
-r5init = [1e9;1.2e9;-6e8];  v5init = [80;-900;50];
-svinit = [r1init;v1init;r2init;v2init;r3init;v3init;r4init;v4init;r5init;v4init];
-% You can hardcode this. svinit and m are the only things needed
+mSun = 1.989e30;
+m = [mSun/3 mSun/3 mSun/3 mSun/3];
+r1init = [1e9;0]; r2init = [0;1e8]; r3init = [-1e9;0];  r4init = [0;-1e8];
+v1init = [0;-5]; v2init = [4.6;0];  v3init = [0;5]; v4init = [-4.6;0];
+svinit = [r1init;v1init;r2init;v2init;r3init;v3init;r4init;v4init];
+% you can hardcode this. Base distance unit is km
 
-%---Sim Times--------------------------------------------------------------
-tEnd = seconds(years(2));           % End time
-tScale = seconds(years(1/12));      % Sim timescale. How long in-sim per second irl?
-dt = seconds(days(1/2));            % How long between each calculated frame. Mostly just to make sure that lines appear smooth. Must be < tscale/framerate
-fps = 30;                           % Framerate of the animation
-rotrate = 15;                       % deg/sec pan rate for the animation
+tEnd = seconds(years(50));    % sim for 50 years
+tScale = seconds(years(2));   % aim for a year per second
+dt = seconds(years(1/200));   % 200 frames per year
+fps = 30;
 
-useCoM = true;                      % whether to use CoM as origin. Unless you know what you're doing, keep this true
-useTex = true;                      % Whether to use LaTeX labels (otherwise matlab Tex used). Currenty, "false" will lead to stupid looking plots
-
+useCoM = true;  % whether to use CoM as origin
+useTex = true;  % whether to use tex labels (otherwise MATLAB labels)
 % Note: search for ANIMATIONCOMMENT for stuff to toggle based on whether
-% saving the animation or not
+% animating or not
 
 %% run sim
+
 % came to me in a fever dream. Prevents the spacing between frames from
 % being a non-integer
 % Comes from finding the dt required to make spacing an integer. Done so by
@@ -34,28 +28,28 @@ useTex = true;                      % Whether to use LaTeX labels (otherwise mat
 dt = (tEnd/(fps*tEnd/tScale))/ceil((tEnd/(fps*tEnd/tScale))/dt);
 
 numbods = length(m);
-if(numbods ~= length(svinit)/6)
+if(numbods ~= length(svinit)/4)
     error("Improper inputs. Check to make sure your state vector has 4x as many elements as your masses");
 end
 
 ops = odeset('RelTol', 1e-10, 'AbsTol', 1e-5);
 [t,SV] = ode45(@(t,sv) dynamics(t,sv, m, numbods), 0:dt:tEnd, svinit, ops);
 
-x=zeros(length(t),numbods);y=zeros(length(t),numbods);z=zeros(length(t),numbods);
-vx=zeros(length(t),numbods);vy=zeros(length(t),numbods);vz=zeros(length(t),numbods);
+x = zeros(length(t),numbods);
+y = zeros(length(t),numbods);
+vx = zeros(length(t),numbods);
+vy = zeros(length(t),numbods);
 for idx = 1:numbods
-    x(:,idx) = SV(:,idx*6-5);
-    y(:,idx) = SV(:,idx*6-4);
-    z(:,idx) = SV(:,idx*6-3);
-    vx(:,idx) = SV(:,idx*6-2);
-    vy(:,idx) = SV(:,idx*6-1);
-    vz(:,idx) = SV(:,idx*6);
+    x(:,idx) = SV(:,idx*4-3);
+    y(:,idx) = SV(:,idx*4-2);
+    vx(:,idx) = SV(:,idx*4-1);
+    vy(:,idx) = SV(:,idx*4);
 end
 
 if(useCoM)
     M=sum(m);
-    xc=sum(x.*m,2)/M; yc=sum(y.*m,2)/M; zc=sum(z.*m,2)/M; vxc=sum(vx.*m,2)/M; vyc=sum(vy.*m,2)/M; vzc=sum(vz.*m,2)/M;
-    x = x-xc;   y = y-yc;   z=z-zc; vx = vx-vxc;    vy = vy-vyc; vz=vz-vzc;
+    xc=sum(x.*m,2)/M; yc=sum(y.*m,2)/M; vxc=sum(vx.*m,2)/M; vyc=sum(vy.*m,2)/M;
+    x = x-xc;   y = y-yc;   vx = vx-vxc;    vy = vy-vyc;
 end
 if(useTex)
     interp = "latex";
@@ -64,39 +58,34 @@ else
 end
 
 %% Paths
-figure;
-plot3(x, y, z, '-'); set(gcf, "color", 'w');
-lgtxt = {};
+plot(x, y, '-');
+lgtxt = [];
 for idx=1:numbods
-    mExp = floor(log10(m(idx))); mCoef = round(m(idx)/10^mExp, 3);
-    lgtxt{end+1} = "$$"+mCoef+"\times 10^{"+mExp+"}$$";
+    lgtxt = [lgtxt, "$$r_{"+idx+"}(t)$$"];
 end
-lg=legend(lgtxt,Interpreter=interp,Location="eastoutside", box="off");
-lg.ItemTokenSize=[10 0]; lg.Title.String="Masses [kg]"; 
-set(gca,'TickLabelInterpreter',interp)
-title(numbods+"-Body Sim", Interpreter=interp); axis equal;grid on;
-[az, el] = view; xl = xlim; yl=ylim; zl=zlim;
+legend(lgtxt, Interpreter=interp); set(gca,'TickLabelInterpreter',interp)
+title("Body Locations", Interpreter=interp); axis equal;
+xlabel("X Position ($$x$$) [km]", Interpreter=interp); ylabel("Y Position ($$y$$) [km]", Interpreter=interp); grid on;
+xl = xlim; yl=ylim;
 %% Animation
-figure; set(gcf, "color", 'w');
-objs = plot3(xl',yl',zl', 'ko'); animAx = gca; colors = get(gca, 'ColorOrder');
-axis equal; grid;
-title(numbods+"-Body Sim", Interpreter=interp); subtitle("$$t=0$$ years", Interpreter=interp);
+figure;
+objs = plot(nan,nan, 'ko'); animAx = gca; colors = get(gca, 'ColorOrder');
+xlim(xl); ylim(yl);
+title("Body Locations", Interpreter=interp); subtitle("$$t=0$$ years", Interpreter=interp);
+xlabel("X Position ($$x$$) [km]", Interpreter=interp); ylabel("Y Position ($$y$$) [km]", Interpreter=interp);
 set(gca,'TickLabelInterpreter',interp)
 for idx=1:numbods
     al(idx) = animatedline("LineWidth", 0.1,"Color",colors(idx,:));
 end
-lg=legend(["", lgtxt],Interpreter=interp,Location="eastoutside", box="off");
-lg.ItemTokenSize=[10 0]; lg.Title.String="Masses [kg]";
-pause; tic; xlim(xl);ylim(yl);zlim(zl);
-animation = VideoWriter("animation3d_"+string(datetime, "yyyy-MM-dd-hhmmss")+".mp4",'MPEG-4');      % ANIMATIONCOMMENT
+pause; tic;
+
+animation = VideoWriter("animation"+string(datetime, "yyyy-MM-dd-hhmmss")+".mp4",'MPEG-4');      % ANIMATIONCOMMENT
 animation.FrameRate = fps; animation.Quality=100; open(animation);      % ANIMATIONCOMMENT
 sp = (length(t)-1)/(fps*tEnd/tScale); % frames (sim) per frame (display)
 for idx=sp:sp:length(t)
-    az = az + rotrate/fps;
-    view(az, el);
-    set(objs, "XData", x(idx,:), "YData", y(idx,:), "ZData", z(idx,:));
+    set(objs, "XData", x(idx,:), "YData", y(idx,:));
     for alIdx=1:length(al)
-        addpoints(al(alIdx), x(idx+(1-sp:0),alIdx), y(idx+(1-sp:0),alIdx),  z(idx+(1-sp:0),alIdx));
+        addpoints(al(alIdx), x(idx+(1-sp:0),alIdx), y(idx+(1-sp:0),alIdx));
     end
     animAx.Subtitle.String = sprintf("$$t=%.2f$$ years", years(seconds(t(idx))));
     %pause(max([1/fps-toc, 0]));tic;   % ANIMATIONCOMMENT
@@ -108,16 +97,16 @@ close(animation);   % ANIMATIONCOMMENT
 %% Helper function
 % it works, don't touch
 function dsv = dynamics(t, sv, m, num)
-G = 6.6743e-11;
-dynMtx = zeros(6*num,6*num);
+G = 6.6743e-20; % division to convert to km as base distance unit
+dynMtx = zeros(4*num,4*num);
 for targetnum = 1:num
-    dynMtx((targetnum-1)*6+[1:3],(targetnum-1)*6+[4:6]) = eye(3);
+    dynMtx((targetnum-1)*4+[1,2],(targetnum-1)*4+[3,4]) = eye(2);
     for sourcenum = 1:num
-        d = norm(sv((1:3)+(sourcenum-1)*6) - sv((1:3)+(targetnum-1)*6));
-        gmd3 = (G*m(sourcenum)/d^3)*eye(3);
+        d = norm(sv((1:2)+(sourcenum-1)*4) - sv((1:2)+(targetnum-1)*4));
+        gmd3 = (G*m(sourcenum)/d^3)*eye(2);
         if(targetnum ~= sourcenum)
-            dynMtx((targetnum-1)*6+[4:6],(sourcenum-1)*6+[1:3]) = gmd3;
-            dynMtx((targetnum-1)*6+[4:6],(targetnum-1)*6+[1:3]) = dynMtx((targetnum-1)*6+[4:6],(targetnum-1)*6+[1:3])-gmd3;
+            dynMtx((targetnum-1)*4+[3,4],(sourcenum-1)*4+[1,2]) = gmd3;
+            dynMtx((targetnum-1)*4+[3,4],(targetnum-1)*4+[1,2]) = dynMtx((targetnum-1)*4+[3,4],(targetnum-1)*4+[1,2])-gmd3;
         end
     end
 end
